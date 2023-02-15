19cfc261c6f17d48cfa38996f7435dbb207d4eec
Margarita <m.novolokova@gmail.com>
2023-02-15 22:13:13 +0200

    Add inline styles

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -2,10 +2,15 @@ import React from "react";
 import SignUpForm from "../components/forms/SignUpForm";
 
 const SignUpPage = () => {
+  const styleTitle={
+    width: "45%",
+    margin: "auto",
+    marginBottom: "20px"
+  }
   return (
     <>
       <h1>CREATE AN ACCOUNT</h1>
-      <h2 style={{ width: "45%", margin: "auto", marginBottom: "20px"}}>We always keep your name and email address private.</h2>
+      <h2 style={styleTitle}>We always keep your name and email address private.</h2>
       <SignUpForm />
     </>
   );
